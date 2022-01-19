bb2ce267c9c2e7a23ee100803bca9ee89a69b1b3
Christian <christianvillere@gmail.com>
2022-01-19 11:56:16 -0800

    Refactor determine_game_result a bit

--- main.rb
+++ main.rb
@@ -1,27 +1,5 @@
 # frozen_string_literal: true
-
-# Project to build the Hangman game for Odin Project
-
-=begin
-###Pre-writing for Hangman
-VERBS
-1) Load in 5desk.txt file
-2) Randomly select secret word between 5 and 12 characters long from file
-3) Make guess of a letter (case insensitive)
----puts phrase to ask player to make a letter guess
----receive it
----check it against every letter in word
------good: "correct letter guess"
------bad: "incorrect. you have #{@@max_guesses} left"
-4) Update displays if letter was correct or incorrect
-5) Check if player is out of guesses
-6) Check if player has won the game
-
-NOUNS
-1) A Display of how many guesses left before you lose (class variable)
-2) A Display of correct letters and their position on the board (instance variable)
-3) A Display of the incorrect letters already chosen (instance variable)
-=end
+require 'yaml'
 
 # Main class to play hangman game
 class Hangman
@@ -33,12 +11,13 @@ class Hangman
     @incorrect_letters = []
     @word = ''
     @word_array = ''
-    @max_guesses = 3
+    @max_guesses = 9
   end
 
   def generate_random_word
     all_words = File.read('/Users/christianvillere/the_odin_project/Ruby/hangman/5desk.txt').split
     @word = all_words.select { |n| n.length >= 5 && n.length <= 12 }.sample(1)
+    puts "random word: #{@word}"
     create_word_array.each { @correct_letters.push('_') }
     display_correct_letters
   end
@@ -64,7 +43,6 @@ class Hangman
         @correct_letters[index] = player_guess
       end
     end
-    @max_guesses -= 1
     display_correct_letters
   end
 
@@ -92,6 +70,7 @@ class Hangman
 
   def determine_game_result
     if @max_guesses.positive? == true && @correct_letters.include?('_') == false
+      puts "#{@correct_letters.join(' ')}  is...#{@correct_letters.join('').upcase}!!"
       puts 'You have won the game! Would you like to play again? 1 for Yes & 2 for No'
       play_another_game
     end
@@ -110,7 +89,8 @@ class Hangman
       end
     end
   end
+
 end
 
-my_class = Hangman.new
-my_class.generate_random_word
+
+Hangman.new.generate_random_word
